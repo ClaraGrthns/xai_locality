2725345ead9aaffe0e7cdea48ddf449757821e1c
ClaraGrthns <ClaraGrthns>
2025-01-02 14:22:11 +0100

    fix buggs in code

--- plot_max_accuracies.py
+++ plot_max_accuracies.py
@@ -1,26 +1,15 @@
 import argparse
 import numpy as np
 from utils.plotting_utils import plot_accuracy_vs_fraction
-from model.lightgbm import load_model, load_data, predict_fn
-from functools import partial
 import os.path as osp
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Plot accuracy vs fraction for different kernel widths.")
-    parser.add_argument('--kernel_widths', nargs='+', type=float, required=True, help='List of kernel widths.')
-    parser.add_argument('--result_dir', type=str, required=True, help='Directory to save results.')
-    parser.add_argument('--setting', type=str, required=True, help='Setting string.')
-    parser.add_argument('--model_path', type=str, required=True, help='Path to the model.')
-    parser.add_argument('--data_path', type=str, required=True, help='Path to the data.')
-    parser.add_argument("--data_folder", type=str, help="Path to the data folder")
-    parser.add_argument("--model_folder", type=str, help="Path to the model folder")
+    parser.add_argument('--kernel_widths', nargs='+', type=float, default = [1.0, 2.5, 5.3, 16.0],   help='List of kernel widths.')
     parser.add_argument("--results_folder", type=str, help="Path to the results folder")
-    parser.add_argument("--model_type", type=str, default="lightgbm", help="Model type, so far only 'xgboost' and 'lightgbm' is supported")
     parser.add_argument("--results_path", type=str, help="Path to save results", default="/home/grotehans/xai_locality/results/lightgbm/jannis")
-    parser.add_argument("--distance_measure", type=str, default="euclidean", help="Distance measure")
-    parser.add_argument("--experiment_setting_threshold", type=str, help="Leave kernel_width as {kw}")
-    parser.add_argument("--num_tresh", type=int, default=150, help="Number of thresholds")
-    parser.add_argument("--model_regressor", type=str, default="ridge", help="Model regressor for LIME")
+    parser.add_argument('--setting', type=str,  help='Setting string.')
+    parser.add_argument("--experiment_setting", type=str, default = "thresholds-0-4.0-max63.0num_tresh-150_only_test_kernel_width-{kw}_model_regr-ridge_model_type-lightgbm_accuracy_fraction.npy.npz", help="Leave kernel_width as")
     return parser.parse_args()
 
 def load_results(data_path):
@@ -48,6 +37,7 @@ def load_and_get_non_zero_cols(data_path):
 
 def main():
     args = parse_args()
+    print(args)
     def get_path(base_folder, base_path, setting, suffix=""):
         if base_folder is None:
             return base_path
@@ -55,12 +45,12 @@ def main():
         return osp.join(base_folder, f"{suffix}{setting}")
     
     results_path = get_path(args.results_folder, args.results_path, args.setting)
-    result_paths = [osp.join(results_path, args.experiment_setting.format(kw)) for kw in args.kernel_widths]
+    experiment_result_paths = [osp.join(results_path, args.experiment_setting.format(kw=kw)) for kw in args.kernel_widths]
     accuracy_arrays = []
     fraction_arrays = []
     non_zero_cols_list = []
 
-    for result_path in result_paths:
+    for result_path in experiment_result_paths:
         accuracy_array, fraction_array, _, non_zero_cols = load_and_get_non_zero_cols(result_path)
         accuracy_arrays.append(accuracy_array)
         fraction_arrays.append(fraction_array)
@@ -68,15 +58,30 @@ def main():
 
     min_cols = min(non_zero_cols_list)
     accuracies_concat = np.stack([acc[:, :min_cols] for acc in accuracy_arrays], axis=0)
+    unique_arrays = set([tuple(arr.flatten()) for arr in accuracies_concat])
+
+    print(f"Number of unique arrays: {len(unique_arrays)}")
+    print(f"Expected length: {len(accuracy_arrays)}")
+    if len(unique_arrays) != len(accuracy_arrays):
+        print("Warning: Some arrays are identical!")
+    else:
+        print("All arrays are unique.")
+
     fraction_concat = np.stack([frac[:, :min_cols] for frac in fraction_arrays], axis=0)
 
     max_accuracies = np.max(accuracies_concat, axis=0)
     max_indices = np.argmax(accuracies_concat, axis=0)
+    print("unqiue values of kernel indices", np.unique(max_indices))
     selected_fractions = fraction_concat[max_indices, np.arange(fraction_concat.shape[1])[:, None], np.arange(fraction_concat.shape[2])]
-    selected_fractions.shape
 
     kernel_id_to_width = {i: str(kw) for i, kw in enumerate(args.kernel_widths)}
-    plot_accuracy_vs_fraction(max_accuracies, selected_fractions, kernel_ids=max_indices, kernel_id_to_width=kernel_id_to_width, title_add_on=" - max over all thresholds")
+    graphics_path = osp.join(results_path, "graphics", f"max_accuracy_vs_fraction_kernel.pdf")
+    plot_accuracy_vs_fraction(max_accuracies, selected_fractions, 
+                              kernel_ids=max_indices, 
+                              kernel_id_to_width=kernel_id_to_width, 
+                              title_add_on=" - max over all thresholds",
+                              save_path=graphics_path)
+    print("Saved plot to", graphics_path)
 
 if __name__ == "__main__":
     main()
