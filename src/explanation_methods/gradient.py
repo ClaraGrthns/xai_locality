7f61a79e7ab9420a8594e302bf25e89b015fe2ae
ClaraGrthns <ClaraGrthns>
2025-02-25 11:08:40 +0100

    adapted script to unify gradient and lime methods

--- src/explanation_methods/gradient.py
+++ src/explanation_methods/gradient.py
@@ -1,7 +1,7 @@
 from src.explanation_methods.base import BaseExplanationMethodHandler
 from captum.attr import IntegratedGradients, NoiseTunnel
 import torch
-from src.explanation_methods.gradient_methods.local_classifier import compute_gradmethod_mse_per_fraction, compute_saliency_maps
+from src.explanation_methods.gradient_methods.local_classifier import compute_gradmethod_fidelity_per_kNN, compute_saliency_maps
 import os.path as osp
 import os
 import h5py
@@ -19,6 +19,7 @@ class IntegratedGradientsHandler(BaseExplanationMethodHandler):
     
     
     def compute_explanations(self, results_path, predict_fn, tst_data):
+        tst_feat_for_expl_loader = DataLoader(tst_data, batch_size=self.args.chunk_size, shuffle=False)
         device = torch.device("cpu")
         saliency_map_folder = osp.join(results_path, 
                                         "saliency_maps")
@@ -30,7 +31,7 @@ class IntegratedGradientsHandler(BaseExplanationMethodHandler):
         else:
             if not osp.exists(saliency_map_folder):
                 os.makedirs(saliency_map_folder)
-            saliency_maps = compute_saliency_maps(self.explainer, predict_fn, tst_data, device)
+            saliency_maps = compute_saliency_maps(self.explainer, predict_fn, tst_feat_for_expl_loader)
             with h5py.File(saliency_map_file_path, "w") as f:
                 f.create_dataset("saliency_map", data=saliency_maps.cpu().numpy())
         return saliency_maps
@@ -38,16 +39,16 @@ class IntegratedGradientsHandler(BaseExplanationMethodHandler):
     def get_experiment_setting(self, fractions):
         return f"fractions-{np.round(fractions[0], 2)}-{np.round(fractions[-1], 2)}_grad_method-{self.args.gradient_method}_model_type-{self.args.model_type}_accuracy_fraction.npy"      
     
-    def prepare_data_for_analysis(self, dataset, df_feat):
-        args = self.args
-        indices = np.random.permutation(len(dataset))
-        tst_indices, analysis_indices = np.split(indices, [args.max_test_points])
-        print("using the following indices for testing: ", tst_indices)
-        tst_data = Subset(dataset, tst_indices)
-        analysis_data = Subset(dataset, analysis_indices)
-        tst_feat, analysis_feat = np.split(df_feat[indices], [args.max_test_points])
-        data_loader_tst = DataLoader(tst_data, batch_size=args.chunk_size, shuffle=False)
-        return tst_feat, analysis_feat, data_loader_tst, analysis_data
+    # def prepare_data_for_analysis(self, dataset, df_feat):
+    #     args = self.args
+    #     indices = np.random.permutation(len(dataset))
+    #     tst_indices, analysis_indices = np.split(indices, [args.max_test_points])
+    #     print("using the following indices for testing: ", tst_indices)
+    #     tst_data = Subset(dataset, tst_indices)
+    #     analysis_data = Subset(dataset, analysis_indices)
+    #     tst_feat, analysis_feat = np.split(df_feat[indices], [args.max_test_points])
+    #     data_loader_tst = DataLoader(tst_data, batch_size=args.chunk_size, shuffle=False)
+    #     return tst_feat, analysis_feat, data_loader_tst, analysis_data
 
     def iterate_over_data(self, 
                      tst_feat_for_expl, 
@@ -63,7 +64,10 @@ class IntegratedGradientsHandler(BaseExplanationMethodHandler):
         
         
         chunk_size = self.args.chunk_size
-        for i, (imgs,_,_) in enumerate(tst_feat_for_expl):
+        tst_feat_for_expl_loader = DataLoader(tst_feat_for_expl, batch_size=chunk_size, shuffle=False)
+
+        for i, batch in enumerate(tst_feat_for_expl_loader):
+            tst_chunk = batch[0]
             chunk_start = i*chunk_size
             chunk_end = min(chunk_start + chunk_size, len(tst_feat_for_dist))
             print(f"Computing mse/var for chunk {i} from {chunk_start} to {chunk_end}")
@@ -72,26 +76,34 @@ class IntegratedGradientsHandler(BaseExplanationMethodHandler):
                 print(f"Computing mse/var for {n_closest} closest points")
                 # top_labels = torch.argmax(predict_fn(imgs), dim=1).tolist()
                 with torch.no_grad():
-                    predictions = predict_fn(imgs) #shape: num test samples x num classes
-                    predictions_baseline = predict_fn(torch.zeros_like(imgs)) #
-                chunk_result = compute_gradmethod_mse_per_fraction(tst_feat_for_dist[chunk_start:chunk_end], 
-                                                                   imgs,
-                                                                                    predictions,
-                                                                                    predictions_baseline,
-                                                                                    df_feat_for_expl, 
-                                                                                    explanations_chunk, 
-                                                                                    predict_fn, 
-                                                                                    n_closest, 
-                                                                                    tree, 
-                                                                                )
-                n_closest, mse, mse_lin_approx, variance_pred, rad = chunk_result
+                    predictions = predict_fn(tst_chunk) #shape: num test samples x num classes
+                    predictions_baseline = predict_fn(torch.zeros_like(tst_chunk)) 
+                    if predictions.shape[-1] == 1:
+                        predictions_sm = torch.sigmoid(predictions)
+                        predictions_sm = torch.cat([1 - predictions_sm, predictions_sm], dim=-1)
+                    else:
+                        predictions_sm = torch.softmax(predictions, dim=-1)
+                    top_labels = torch.argmax(predictions_sm, dim=1).tolist()
+                chunk_result = compute_gradmethod_fidelity_per_kNN(tst_feat_for_dist[chunk_start:chunk_end], 
+                                                                   tst_chunk,
+                                                                    predictions,
+                                                                    predictions_baseline,
+                                                                    df_feat_for_expl, 
+                                                                    explanations_chunk, 
+                                                                    predict_fn, 
+                                                                    n_closest, 
+                                                                    tree,
+                                                                    top_labels 
+                                                                )
+                n_closest, mse, accuracy, variance_pred, rad = chunk_result
                 fraction_idx = np.where(n_points_in_ball == n_closest)[0][0]
                 results["mse"][fraction_idx, chunk_start:chunk_end] = mse.cpu().numpy()
-                results["mse_lin_approx"][fraction_idx, chunk_start:chunk_end] = mse_lin_approx.cpu().numpy()
+                results["accuracy"][fraction_idx, chunk_start:chunk_end] = accuracy.cpu().numpy()
                 results["variance_pred"][fraction_idx, chunk_start:chunk_end] = variance_pred.cpu().numpy()
                 results["radius"][fraction_idx, chunk_start:chunk_end ] = rad
                 print(f"Finished computing mse/var for {n_closest} closest points")
             np.savez(osp.join(results_path, experiment_setting), **results)
+        return results
 
 
         
--- src/explanation_methods/lime.py
+++ src/explanation_methods/lime.py
@@ -1,38 +1,40 @@
 from src.explanation_methods.base import BaseExplanationMethodHandler
 import lime.lime_tabular
-from src.explanation_methods.lime_analysis.lime_local_classifier import compute_lime_accuracy_per_fraction, compute_explanations
+from src.explanation_methods.lime_analysis.lime_local_classifier import compute_lime_fidelity_per_kNN, compute_explanations
 from src.utils.misc import get_path
 import os.path as osp
 import os
 import numpy as np
 from joblib import Parallel, delayed
+from torch.utils.data import Subset, DataLoader
+import time
 
 class LimeHandler(BaseExplanationMethodHandler):
     def set_explainer(self, **kwargs):
         args = self.args
-        trn_feat = kwargs.get('dataset')[4]
+        trn_feat = kwargs.get('dataset')
         class_names = kwargs.get('class_names')
-        return lime.lime_tabular.LimeTabularExplainer(trn_feat, 
+        self.explainer = lime.lime_tabular.LimeTabularExplainer(trn_feat, 
                                                     feature_names=np.arange(trn_feat.shape[1]),
                                                       class_names=class_names, 
                                                       discretize_continuous=True, 
-                                                      random_state=args.random_state, 
+                                                      random_state=args.random_seed, 
                                                       kernel_width=args.kernel_width)
     
     def explain_instance(self, **kwargs):
         return self.explainer.explain_instance(**kwargs)
 
     def compute_accuracy(self, tst_set, analysis_dataset, explanations, explainer, predict_fn, n_closest, tree, pred_threshold, top_labels=None):
-        return compute_lime_accuracy_per_fraction(tst_set, analysis_dataset, explanations, explainer, predict_fn, n_closest, tree, pred_threshold)
+        return compute_lime_fidelity_per_kNN(tst_set, analysis_dataset, explanations, explainer, predict_fn, n_closest, tree, pred_threshold)
     
     def compute_explanations(self, results_path, predict_fn, tst_data):
         args = self.args
         # Construct the explanation file name and path
         explanation_file_name = f"normalized_data_explanations_test_set_kernel_width-{args.kernel_width}_model_regressor-{args.model_regressor}"
-        if args.num_lime_features > 10:
-            explanation_file_name += f"_num_features-{args.num_lime_features}"
-        if args.num_test_splits > 1:
-            explanation_file_name = f"split-{args.split_idx}_{explanation_file_name}"
+        # if args.num_lime_features > 10:
+        #     explanation_file_name += f"_num_features-{args.num_lime_features}"
+        # if args.num_test_splits > 1:
+        #     explanation_file_name = f"split-{args.split_idx}_{explanation_file_name}"
         explanations_dir = osp.join(results_path, "explanations")
         explanation_file_path = osp.join(explanations_dir, explanation_file_name)
         print(f"using explanation path: {explanation_file_path}")
@@ -45,6 +47,7 @@ class LimeHandler(BaseExplanationMethodHandler):
             explanations = np.load(explanation_file_path+".npy", allow_pickle=True)
             print(f"{len(explanations)} explanations loaded")
         else:
+            tst_data = tst_data.features
             print("Precomputed explanations not found. Computing explanations for the test set...")
             explanations = compute_explanations(self.explainer, tst_data, predict_fn, args.num_lime_features)
             
@@ -56,24 +59,23 @@ class LimeHandler(BaseExplanationMethodHandler):
     def get_experiment_setting(self, fractions):
         args = self.args
         df_setting = "complete_df" if args.include_trn and args.include_val else "only_test"
-
         experiment_setting = f"fractions-{0}-{np.round(fractions[-1])}_{df_setting}_kernel_width-{args.kernel_width}_model_regr-{args.model_regressor}_model_type-{args.model_type}_accuracy_fraction.npy"
-        if args.num_lime_features > 10:
-            experiment_setting = f"num_features-{args.num_lime_features}_{experiment_setting}"
-        if args.num_test_splits > 1:
-            experiment_setting = f"split-{args.split_idx}_{experiment_setting}"
+        # if args.num_lime_features > 10:
+        #     experiment_setting = f"num_features-{args.num_lime_features}_{experiment_setting}"
+        # if args.num_test_splits > 1:
+        #     experiment_setting = f"split-{args.split_idx}_{experiment_setting}"
         return experiment_setting
     
-    def prepare_data_for_analysis(self, dataset, df_feat):
-        args = self.args
-        tst_feat, _, val_feat, _, trn_feat, _  = dataset
-        df_feat = tst_feat[args.max_test_points:]
-        tst_feat = tst_feat[: args.max_test_points]
-        if args.include_trn:
-            df_feat = np.concatenate([trn_feat, df_feat], axis=0)
-        if args.include_val:
-            df_feat = np.concatenate([df_feat, val_feat], axis=0)
-        return tst_feat, df_feat, tst_feat, df_feat
+    # def prepare_data_for_analysis(self, dataset, df_feat):
+    #     args = self.args
+    #     tst_feat, _, val_feat, _, trn_feat, _  = dataset
+    #     df_feat = tst_feat[args.max_test_points:]
+    #     tst_feat = tst_feat[: args.max_test_points]
+    #     if args.include_trn:
+    #         df_feat = np.concatenate([trn_feat, df_feat], axis=0)
+    #     if args.include_val:
+    #         df_feat = np.concatenate([df_feat, val_feat], axis=0)
+    #     return tst_feat, df_feat, tst_feat, df_feat
     
     def iterate_over_data(self,
                      tst_feat_for_expl, 
@@ -86,46 +88,51 @@ class LimeHandler(BaseExplanationMethodHandler):
                      results_path,
                      experiment_setting,
                      results):
+        print(len(tst_feat_for_expl))
+        print(len(df_feat_for_expl))
         chunk_size = self.args.chunk_size
         predict_threshold = self.args.predict_threshold
-        
-        for i in range(0, len(tst_feat_for_expl), chunk_size):
+        tst_feat_for_expl_loader = DataLoader(tst_feat_for_expl, batch_size=chunk_size, shuffle=False)
+        for i, batch in enumerate(tst_feat_for_expl_loader):
+            tst_chunk = batch[0].numpy()
+            chunk_start = i * chunk_size
+            chunk_end = min(chunk_start + chunk_size, len(df_feat_for_expl))
+            print(f"Processing chunk {i // chunk_size + 1}/{(len(tst_feat_for_expl) + chunk_size - 1) // chunk_size}")
+            chunk_end = min(i + chunk_size, len(tst_feat_for_expl))
+            explanations_chunk = explanations[chunk_start:chunk_end]
+
             if self.args.debug:
                 # Normal for loop for easier debugging
-                chunk_end = min(i + chunk_size, len(tst_feat_for_expl))
-                explanations_chunk = explanations[i:chunk_end]
                 for n_closest in n_points_in_ball:
-                    print(f"Processing fraction {n_closest}")
-                    chunk_results = compute_lime_accuracy_per_fraction(
-                        tst_feat_for_expl[i:i+chunk_size], df_feat_for_expl, explanations_chunk, self.explainer, predict_fn, n_closest, tree, predict_threshold
+                    start_time = time.time()
+                    print(f"Processing {n_closest} nearest neighbours")
+                    n_closest, acc, mse_local, ratio, R = compute_lime_fidelity_per_kNN(
+                    tst_chunk, df_feat_for_expl, explanations_chunk, self.explainer, predict_fn, n_closest, tree, predict_threshold
                     )
                     fraction_idx = np.where(n_points_in_ball == n_closest)[0][0]
-                    results["accuracy"][fraction_idx, i:i+chunk_size] = chunk_results[1]
-                    results["radius"][fraction_idx, i:i+chunk_size] = chunk_results[-1]
-                np.savez(osp.join(results_path, experiment_setting), **results)
+                    results["accuracy"][fraction_idx, chunk_start:chunk_end] = acc
+                    results["ratio_all_ones"][fraction_idx, chunk_start:chunk_end] = ratio
+                    results["radius"][fraction_idx, chunk_start:chunk_end] = R
+                    results["mse"][fraction_idx, chunk_start:chunk_end] = mse_local
+                    print(f"Time taken: {time.time() - start_time}")
             else:
-                print(f"Processing chunk {i//chunk_size + 1}/{(len(tst_feat_for_expl) + chunk_size - 1)//chunk_size}")
-                # Parallel processing for normal execution
-                chunk_end = min(i + chunk_size, len(tst_feat_for_expl))
-                tst_chunk = tst_feat_for_expl[i:chunk_end]
-                explanations_chunk = explanations[i:chunk_end]
                 chunk_results = Parallel(n_jobs=-1)(
-                        delayed(compute_lime_accuracy_per_fraction)(
-                            tst_chunk, df_feat_for_expl, explanations_chunk, self.explainer, predict_fn, n_closest, tree, predict_threshold
-                        )
-                        for n_closest in n_points_in_ball
+                    delayed(compute_lime_fidelity_per_kNN)(
+                    tst_chunk, df_feat_for_expl, explanations_chunk, self.explainer, predict_fn, n_closest, tree, predict_threshold
                     )
+                    for n_closest in n_points_in_ball
+                )
                 # Unpack results directly into the correct positions in the arrays
-                for n_closest, acc, ratio, R in chunk_results:
+                for n_closest, acc, mse_local, ratio, R in chunk_results:
                     fraction_idx = np.where(n_points_in_ball == n_closest)[0][0]
-                    results["accuracy"][fraction_idx, i:chunk_end] = acc
-                    results["ratio_all_ones"][fraction_idx, i:chunk_end] = ratio
-                    results["radius"][fraction_idx, i:chunk_end] = R
+                    results["accuracy"][fraction_idx, chunk_start:chunk_end] = acc
+                    results["ratio_all_ones"][fraction_idx, chunk_start:chunk_end] = ratio
+                    results["radius"][fraction_idx, chunk_start:chunk_end] = R
+                    results["mse"][fraction_idx, chunk_start:chunk_end] = mse_local
 
             np.savez(osp.join(results_path, experiment_setting), **results)
-            print(f"Processed chunk {i//chunk_size + 1}/{(len(tst_feat_for_expl) + chunk_size - 1)//chunk_size}")
-    
-
-
+            print(f"Processed chunk {i // chunk_size + 1}/{(len(tst_feat_for_expl) + chunk_size - 1) // chunk_size}")
+                
+        return results
             
 
