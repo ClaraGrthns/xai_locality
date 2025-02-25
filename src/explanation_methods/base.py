2d92e948ed781e34b7153d2a4a871b80bf255f60
ClaraGrthns <ClaraGrthns>
2025-02-25 11:07:53 +0100

    unified data handling across methods

--- src/explanation_methods/base.py
+++ src/explanation_methods/base.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+from torch.utils.data import DataLoader, Subset
+
 class BaseExplanationMethodHandler:
     def __init__(self,args):
         # self.explainer = self.set_explainer(**kwargs)
@@ -20,9 +22,24 @@ class BaseExplanationMethodHandler:
     def get_experiment_setting(self):
         raise NotImplementedError
     
-    def prepare_data_for_analysis(self):
-        raise NotImplementedError
+    def prepare_data_for_analysis(self, dataset, df_feat):
+        args = self.args
+        indices = np.random.permutation(len(dataset))
+        tst_indices, analysis_indices = np.split(indices, [args.max_test_points])
+        print("using the following indices for testing: ", tst_indices)
+        tst_data = Subset(dataset, tst_indices)
+        analysis_data = Subset(dataset, analysis_indices)
+        print("Length of data set for analysis", len(analysis_data))
+        print("Length of test set", len(tst_data))
+        if df_feat is not None:
+            tst_feat, analysis_feat = np.split(df_feat[indices], [args.max_test_points])
+        else:
+            tst_feat, analysis_feat = tst_data.features, analysis_feat.features
+
+        data_loader_tst = DataLoader(tst_data, batch_size=args.chunk_size, shuffle=False)
+        return tst_feat, analysis_feat, data_loader_tst, analysis_data
     
+
     def iterate_over_data(self):
         raise NotImplementedError
     
@@ -48,10 +65,27 @@ class BaseExplanationMethodHandler:
         "variance_pred": np.zeros((num_fractions, self.args.max_test_points)),
         "radius": np.zeros((num_fractions, self.args.max_test_points)),
         "fraction_points_in_ball": fractions,
+        "n_points_in_ball": n_points_in_ball,
         "ratio_all_ones": np.zeros((num_fractions, self.args.max_test_points)),
         }  
+        # chunk_size = self.args.chunk_size
+        # predict_threshold = self.args.predict_threshold
+        # for i, batch in enumerate(tst_feat_for_expl):
+        #     tst_chunk = batch[0]
+        #     chunk_start = i*chunk_size
+        #     chunk_end = min(chunk_start + chunk_size, len(df_feat_for_expl))
+        #     print(f"Processing chunk {i//chunk_size + 1}/{(len(tst_feat_for_expl) + chunk_size - 1)//chunk_size}")
+        #     chunk_end = min(i + chunk_size, len(tst_feat_for_expl))
+        #     explanations_chunk = explanations[chunk_start:chunk_end]
+        #     chunk_results = self.iterate_over_data(tst_chunk = tst_chunk, 
+        #                                            df_feat_for_expl = df_feat_for_expl, 
+        #                                            explanations_chunk = explanations_chunk, 
+        #                                            predict_fn = predict_fn, 
+        #                                            n_points_in_ball = n_points_in_ball, 
+        #                                            tree = tree, 
+        #                                            predict_threshold = predict_threshold)
 
-        self.iterate_over_data(tst_feat_for_expl = tst_feat_for_expl, 
+        results = self.iterate_over_data(tst_feat_for_expl = tst_feat_for_expl, 
                      tst_feat_for_dist = tst_feat_for_dist, 
                      df_feat_for_expl = df_feat_for_expl, 
                      explanations = explanations, 
@@ -61,3 +95,4 @@ class BaseExplanationMethodHandler:
                      results_path = results_path,
                      experiment_setting = experiment_setting,
                      results = results)
+        return results
--- src/model/base.py
+++ src/model/base.py
@@ -6,22 +6,30 @@ from src.utils.misc import get_path
 class BaseModelHandler:
     def __init__(self, args):
         self.args = args
-        if args.method =="lime":
+        if (args.data_path or args.data_folder) is not None:
             self.data_path = self.get_data_path()
+            print(f"Loading data from: {self.data_path}")
+        if (args.model_path or args.model_folder) is not None:
             self.model_path = self.get_model_path()
+            print(f"Loading model from: {self.model_path}")
         self.model = self.load_model()
 
     def get_model_path(self):
-        model_path = get_path(self.args.model_folder, self.args.model_path, self.args.setting)
+        model_path = get_path(self.args.model_folder, self.args.model_path, self.args.setting, suffix="final_model_")
         return model_path
     
     def get_data_path(self):
         data_path = get_path(self.args.data_folder, self.args.data_path, self.args.setting)
+        if self.args.setting is not None:
+            data_path += ".npz"
         return data_path
 
     def load_model(self):
         """Load model from path"""
         raise NotImplementedError
+    
+    def load_feature_vectors(self):
+        return None
 
     def load_data(self):
         """Load dataset"""
